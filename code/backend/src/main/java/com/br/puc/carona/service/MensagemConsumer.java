439e0488e5877ca76517b8cc4c1f7a2c51706a55
GabrielPongelupe <gabrielpongelupee@gmail.com>
2025-04-22 18:50:32 -0300

    fix error: MensagemConsumer

--- code/backend/src/main/java/com/br/puc/carona/service/MensagemConsumer.java
+++ code/backend/src/main/java/com/br/puc/carona/service/MensagemConsumer.java
@@ -1,12 +1,18 @@
 package com.br.puc.carona.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 @Service
+@Slf4j
 @AllArgsConstructor
+@Component
 public class MensagemConsumer {
 
     private final NotificacaoService notificacaoService;
@@ -15,10 +21,10 @@ public class MensagemConsumer {
     // Esse metódo talvez nem continue aqui, mas é um exemplo de como receber mensagens do RabbitMQ
     // e enviar para o WebSocket.
     @RabbitListener(queues = "${app.rabbitmq.queues.notifications}")
-    public void processarMensagem(String mensagem) {
-        System.out.println("Mensagem recebida do RabbitMQ: " + mensagem);
+    public void processarMensagem(Message mensagem) {
+        System.out.println("Mensagem recebida do RabbitMQ: " + mensagem.getPayload());
 
-        // Aqui você envia a mensagem para o WebSocket
-        notificacaoService.enviarNotificacao(mensagem);
+        log.info("MENSAGEM RECEBIDA ");
+        //notificacaoService.enviarNotificacao(mensagem);
     }
 }
