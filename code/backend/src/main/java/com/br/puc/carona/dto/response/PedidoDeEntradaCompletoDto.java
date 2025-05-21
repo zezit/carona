14c0dc3d4b9b49bd96657ecc1cc07d1cb5a0c59e
RenatoMAP77 <renatomapbusiness@gmail.com>
2025-05-21 01:51:17 -0300

    feat: implement endpoint to fetch pending entry requests by driver and add complete DTO #131

---
+++ code/backend/src/main/java/com/br/puc/carona/dto/response/PedidoDeEntradaCompletoDto.java
@@ -0,0 +1,30 @@
+package com.br.puc.carona.dto.response;
+
+import com.br.puc.carona.enums.Status;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PedidoDeEntradaCompletoDto {
+
+    private Long id;
+    private CaronaDto carona;
+    private SolicitacaoCaronaDto solicitacao;
+    private Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String toStringBaseInfo() {
+        return new StringBuilder()
+            .append("PedidoDeEntradaCompletoDto{")
+            .append("id=").append(getId())
+            .append(", carona=").append(carona.toStringBaseInfo())
+            .append(", solicitacao=").append(solicitacao.toStringBaseInfo())
+            .append(", status='").append(status).append('\'')
+            .toString();
+    }
+}
