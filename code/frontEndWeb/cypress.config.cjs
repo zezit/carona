d5b5b29a846d43be9735df54ccc8a52704368b00
Druitti <gabirle.afa@outlook.com>
2025-06-03 22:26:07 -0300

    feat: coverage tests

---
+++ code/frontEndWeb/cypress.config.cjs
@@ -0,0 +1,19 @@
+﻿const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+    video: false,
+    screenshotOnRunFailure: true,
+    
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    }
+  }
+})
--- code/frontEndWeb/vite.config.ts
+++ code/frontEndWeb/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +10,15 @@ export default defineConfig(({ mode }) => ({
     port: 5173,
   },
   plugins: [
-    react(),
+   ...(process.env.NODE_ENV !== 'production' ? [
+      istanbul({
+        include: 'src/*',
+        exclude: ['node_modules', 'test/', '**/*.test.*'],
+        extension: ['.js', '.jsx', '.ts', '.tsx'],
+        requireEnv: false,
+        cypress: true
+      })
+    ] : [])
   ],
   resolve: {
     alias: {
