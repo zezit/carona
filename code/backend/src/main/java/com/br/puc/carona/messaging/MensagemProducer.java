44797a9d38c9ad2d0ed7d0de7dce0cd7706c85cd
José Victor <jvictormmendesd@gmail.com>
2025-06-06 14:50:53 -0300

    refactor: clean up MensagemProducer by removing unused method and organizing imports

--- code/backend/src/main/java/com/br/puc/carona/messaging/MensagemProducer.java
+++ code/backend/src/main/java/com/br/puc/carona/messaging/MensagemProducer.java
@@ -4,9 +4,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.br.puc.carona.messaging.contract.AvaliacaoMessageDTO;
-import com.br.puc.carona.dto.MessageDTO;
 import com.br.puc.carona.dto.request.SolicitacaoCaronaRequest;
+import com.br.puc.carona.messaging.contract.AvaliacaoMessageDTO;
+import com.br.puc.carona.messaging.contract.CancelamentoCaronaPassageiroMessage;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,25 +27,18 @@ public class MensagemProducer {
     @Value("${app.rabbitmq.queues.avaliacoes}")
     private String avaliacoesQueue;
 
-    // Producer básico
-    public void enviarMensagemParaNotifications(MessageDTO mensagem) {
-        log.info("Enviando mensagem para fila '{}': {}", notificationsQueue, mensagem);
-        rabbitTemplate.convertAndSend(notificationsQueue, mensagem);
-        log.info("Enviado para fila");
-    }
-
     public void enviarMensagemParaCaronaRequestQueue(final SolicitacaoCaronaRequest msg) {
         log.info("Enviando para a fila de solicitações de caronas: {}", ridesRequestQueue);
         rabbitTemplate.convertAndSend(ridesRequestQueue, msg);
     }
 
-    /**
-     * Envia uma mensagem para a fila de avaliações
-     *
-     * @param avaliacaoMessage mensagem com dados da avaliação
-     */
     public void enviarMensagemParaAvaliacaoQueue(final AvaliacaoMessageDTO avaliacaoMessage) {
         log.info("Enviando para a fila de avaliações: {}", avaliacoesQueue);
         rabbitTemplate.convertAndSend(avaliacoesQueue, avaliacaoMessage);
+    }
+
+    public void enviarMensagemCancelamentoCaronaPassageiro(final CancelamentoCaronaPassageiroMessage msg) {
+        log.info("Enviando mensagem de cancelamento de carona para passageiro: {}", notificationsQueue);
+        rabbitTemplate.convertAndSend(notificationsQueue, msg);
     }
 }
